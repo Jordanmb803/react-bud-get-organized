75ebcd379054085ff6620b198d63ac00e6d1619d
Jordanmb803 <jordanmb803@gmail.com>
2019-09-23 02:00:37 -0600

    intialized redux store

---
+++ src/ducks/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
+import reducer from './user';
+import promiseMiddleWare from 'redux-promise-middleware';
+
+const store = createStore(reducer, applyMiddleware(promiseMiddleWare))
+
+export default store;
