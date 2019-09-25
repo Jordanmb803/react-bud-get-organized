7a73de9802628d592b5da6dd1b53728685f4aa0d
Jordanmb803 <jordanmb803@gmail.com>
2019-09-25 00:42:40 -0600

    Checkbox working correctly

--- src/components/bill/index.js
+++ src/components/bill/index.js
@@ -10,9 +10,9 @@ class Bill extends Component {
       bill_amount: 0,
       due_date: '',
       paid_amount: 0,
-      paid: 'false',
+      paid: false,
       editable: false,
-      columns: ['name', 'bill_amount', 'due_date', 'paid_amount', 'paid']
+      columns: [['name', 'text'], ['bill_amount', 'text'], ['due_date', 'date'], ['paid_amount', 'text']]
     }
     this.updateBill = this.updateBill.bind(this)
   }
@@ -22,18 +22,14 @@ class Bill extends Component {
       id: this.props.bill.id,
       name: this.props.bill.name,
       bill_amount: this.props.bill.bill_amount,
-      due_date: new Date(this.props.bill.due_date).getDate(),
+      due_date: this.props.bill.due_date.split('T')[0],
       paid_amount: this.props.bill.paid_amount,
-      paid: this.props.bill.paid
+      paid: this.props.bill.paid,
     })
   }
 
   updateBill() {
-    const { id, name, bill_amount, paid_amount, paid } = this.state
-    let year = new Date().getFullYear()
-    let currentMonth = new Date().getMonth()
-
-    let due_date = new Date(year, currentMonth, this.state.due_date)
+    const { id, name, bill_amount, due_date, paid_amount, paid } = this.state
     axios.put(`/bill/update`, {id, name, bill_amount, due_date, paid_amount, paid })
       .then(res => {
         this.setState({
@@ -45,24 +41,36 @@ class Bill extends Component {
   render(){
     return(
       <tr key={this.props.bill.name + this.props.bill.id}>
-        {this.state.columns.map((k, i) => (
-          <td key={k + i}>
-            <input
-              readOnly={!this.state.editable}
-              type="text"
-              value={this.state[k]}
-              onChange={ e => this.setState({[k]: e.target.value})}
-              />
-          </td>
-        ))}
-          <td>
-            <button hidden={this.state.editable} onClick={e => this.setState({editable: true })}>
-              Edit
-             </button>
-            <button hidden={!this.state.editable} onClick={() => this.updateBill()}>
-              DONE
+        {this.state.columns.map((k, i) => {
+          let col = k[0]
+          return(
+            <td key={col + i}>
+              <input
+                readOnly={!this.state.editable}
+                type={k[1]}
+                value={this.state[col]}
+                onChange={ e => this.setState({[col]: e.target.value})}
+                />
+            </td>
+          )
+        })}
+        <td>
+          <input
+            readOnly={!this.state.editable}
+            type="checkbox"
+            value={this.state.paid}
+            checked={this.state.paid}
+            onClick={ e => this.setState({paid: !this.state.paid})}
+          />
+        </td>
+        <td>
+          <button hidden={this.state.editable} onClick={e => this.setState({editable: true })}>
+            Edit
             </button>
-          </td> 
+          <button hidden={!this.state.editable} onClick={() => this.updateBill()}>
+            DONE
+          </button>
+        </td> 
       </tr>
     )
   }
