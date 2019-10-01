c94b8105b26b9af4b4ea5ea34ba2bce022f5e8b1
Jordanmb803 <jordanmb803@gmail.com>
2019-10-01 16:38:42 -0600

    WIP PLAID

--- server/index.js
+++ server/index.js
@@ -8,15 +8,75 @@ const express           = require('express')
     , strategy          = require(`${__dirname}/strategy.js`)
     , bills_controller  = require('./controllers/bills_controller')
     , income_controller = require('./controllers/income_controller')
+    , plaid             = require('plaid')
 
 const {
   SERVER_PORT,
   CONNECTION_STRING,
   SESSION_SECRET,
+  PLAID_CLIENT_ID,
+  PLAID_PUBLIC_KEY,
+  PLAID_SECRET
 } = process.env
+const plaidClient = new plaid.Client(PLAID_CLIENT_ID, PLAID_SECRET, PLAID_PUBLIC_KEY, plaid.environments.development);
 
 const app = express()
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json())
+
+
+app.post('/plaid_exchange', (req, res) => {
+  var public_token = req.body.public_token;
+ 
+  plaidClient.exchangePublicToken(public_token).then(res => {
+    const access_token = res.access_token;
+ 
+    plaidClient.getAccounts(access_token).then(res => {
+      console.log(res.accounts);
+    });
+  }).catch(err => {
+    // Indicates a network or runtime error.
+    if (!(err instanceof plaid.PlaidError)) {
+      res.sendStatus(500);
+      return;
+    }
+ 
+    // Indicates plaid API error
+    console.log('/exchange token returned an error', {
+      error_type: err.error_type,
+      error_code: res.statusCode,
+      error_message: err.error_message,
+      display_message: err.display_message,
+      request_id: err.request_id,
+      status_code: err.status_code,
+    });
+ 
+    // Inspect error_type to handle the error in your application
+    switch(err.error_type) {
+        case 'INVALID_REQUEST':
+          // ...
+          break;
+        case 'INVALID_INPUT':
+          // ...
+          break;
+        case 'RATE_LIMIT_EXCEEDED':
+          // ...
+          break;
+        case 'API_ERROR':
+          // ...
+          break;
+        case 'ITEM_ERROR':
+          // ...
+          break;
+        default:
+          // fallthrough
+    }
+ 
+    res.sendStatus(500);
+  });
+});
 
 massive(CONNECTION_STRING).then(db => {
   app.set('db', db)
@@ -80,6 +140,6 @@ app.put('/income/update', income_controller.updateIncome)
 app.post('/income/create', income_controller.createIncome)
 app.delete('/income/:id/delete', income_controller.deleteIncome)
 
-app.listen(SERVER_PORT, () => {
+app.listen(8000, () => {
   console.log(`Server listening on port ${SERVER_PORT}`)
 })
